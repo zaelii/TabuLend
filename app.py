02b36f1428ec1ade1713a09aca2f4b3feb898880
Nyedson Lorran <nyedsonlorranoficial@gmail.com>
2024-09-15 16:46:09 -0300

    add padrão proxy para login + finalização do sistema de login e cadastro basico

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
-from flask import Flask, render_template,redirect , request, render_template
+from flask import Flask, render_template,redirect , request
+
 import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'NLETESTE'
 
 DadosLogin = 'DadosLogin.txt'
 
+
+class loginProxy:
+    def __init__(self,DadosLogin):
+        self.DadosLogin = DadosLogin
+
+    def verificarLogin(self,username,password):
+        if not os.path.exists(self.DadosLogin):     #verifica se o arquivo dado existe 
+            return False
+        
+
+        with open(self.DadosLogin, 'r') as f:
+            for linha in f:
+                if f'Usuario: {username}' in linha and f'Senha: {password}' in linha:
+                    
+                    return True
+        return False
+    
+
 @app.route('/')
 def home():
     return render_template('telalogin.html')
+
 
 @app.route('/login', methods=['POST'])
 def login():
     nome = request.form.get('username')
     senha = request.form.get('password')
-    print(nome)
-    print(senha)
-    return render_template('catalogo.html')
+
+    Proxy = loginProxy(DadosLogin)  #cria uma instancia do proxy para verificar o login
+
+    if Proxy.verificarLogin(nome,senha):
+        return render_template('catalogo.html')
+    else: 
+        print('USUARIO OU SENHA INVALIDOS!')
+        return redirect('/') 
+
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -28,12 +54,6 @@ def register():
         f.write(f'Nome: {nome}, Email: {email}, Usuario: {usuario} Senha: {senha}\n')
 
     return render_template('catalogo.html')
-
-if __name__ == '__main__':
-    if not os.path.exists(DadosLogin):
-        with open(DadosLogin, 'w') as f:
-            f.write("Cadastro de Usuários:\n")
-    app.run(debug=True)
 
 
 
@@ -56,6 +76,9 @@ def voltartelainicial():
 def telacadastrar():
     return render_template('telacadastrar.html')
 
-    
+
 if __name__ == '__main__':
+    if not os.path.exists(DadosLogin):
+        with open(DadosLogin, 'w') as f:
+            f.write("Cadastro de Usuários:\n")
     app.run(debug=True)
